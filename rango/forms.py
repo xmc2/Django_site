7343063f66ac01175ff112f3b7e50f2411bd0f63
matt cole <mattcole1093@gmail.com>
2016-08-17 21:31:28 -0400

    CH 7 done ;)

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from rango.models import Page, Category
+
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
         help_text="Please enter the category name.")
@@ -17,6 +18,16 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200,
         help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
     class Meta:
         # Provide an association between the ModelForm and a model
         model = Page
--- rango/urls.py
+++ rango/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^about/', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-    views.show_category, name='show_category'),
+        views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+    views.add_page, name='add_page'),
     ]
