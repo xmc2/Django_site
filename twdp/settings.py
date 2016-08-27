d6b88647ddcff790dceea610707382c157809639
Matt Cole <Your Actual Email>
2016-08-27 13:31:28 -0400

    11 -e

--- twdp/settings.py
+++ twdp/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,6 +120,14 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
-# LOGIN USR
-
-LOGIN_URL = '/rango/login/'
+# If True, users can register
+REGISTRATION_OPEN = True
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successful log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+LOGIN_URL = '/accounts/login/'
--- twdp/urls.py
+++ twdp/urls.py
@@ -1,9 +1,18 @@
 from rango import views
 from django.conf.urls import url, include
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page, #if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),
+        name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
